331491a34ae44ea5e0bd2432a37f2b746c2c5cf9
JerryVW <jerryvw72@gmail.com>
2024-06-19 14:02:09 -0400

    Got modal to work and call to action to flow up from bottom

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
 let services = document.getElementById('services');
 let modalContent = document.getElementById('modal-content');
+let trainingClasses = document.getElementsByClassName('training-classes');
 
 services.addEventListener('mouseover', function () {
 	modalContent.style.display = 'block';
 });
 
-services.addEventListener('mouseout', function () {
-	modalContent.style.display = 'none';
+modalContent.addEventListener('mouseleave', function () {
+	modalContent.classList.add('fadeout');
+
+	modalContent.style.opacity = 0;
+	setTimeout(function () {
+		modalContent.style.display = 'none';
+		modalContent.classList.remove('fadeout');
+		modalContent.style.opacity = 1;
+	}, 500);
 });
