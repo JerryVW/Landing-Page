16f0cd3097020d813d3f21fd0497c3f57adf87ad
JerryVW <jerryvw72@gmail.com>
2024-06-21 00:33:09 -0400

    Working on functionality of modals

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 let services = document.getElementById('services');
 let modalContent = document.getElementById('modal-content');
-let trainingClasses = document.getElementsByClassName('training-classes');
 let getStarted = document.getElementById('get-started');
 let classesInfo = document.getElementById('classes-info');
 let submit = document.getElementById('submit');
@@ -9,6 +8,16 @@ let actionContainer = document.getElementById('action-container');
 services.addEventListener('mouseover', function () {
 	modalContent.style.display = 'block';
 });
+
+// services.addEventListener('mouseleave', function () {
+// 	modalContent.classList.add('fadeout');
+// 	modalContent.style.opacity = 0;
+// 	setTimeout(function () {
+// 		modalContent.style.display = 'none';
+// 		modalContent.classList.remove('fadeout');
+// 		modalContent.style.opacity = 1;
+// 	}, 500);
+// });
 
 modalContent.addEventListener('mouseleave', function () {
 	modalContent.classList.add('fadeout');
@@ -21,18 +30,18 @@ modalContent.addEventListener('mouseleave', function () {
 	}, 500);
 });
 
-getStarted.addEventListener('click', function () {
+getStarted.onclick = function () {
 	classesInfo.style.display = 'block';
 	actionContainer.style.display = 'none';
-});
+};
 
-submit.addEventListener('click', function () {
-	classesInfo.classList.add('fadeout');
+// submit.onclick = function () {
+// 	classesInfo.classList.add('fadeout');
 
-	classesInfo.style.opacity = 0;
-	setTimeout(function () {
-		classesInfo.style.display = 'none';
-		classesInfo.classList.remove('fadeout');
-		classesInfo.style.opacity = 1;
-	}, 500);
-});
+// 	classesInfo.style.opacity = 0;
+// 	setTimeout(function () {
+// 		classesInfo.style.display = 'none';
+// 		modalContent.classList.remove('fadeout');
+// 		classesInfo.style.opacity = 1;
+// 	}, 500);
+// };
