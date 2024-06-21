b87d9b1d6637c01a1faf898710f117eee6c0698d
JerryVW <jerryvw72@gmail.com>
2024-06-21 11:59:40 -0400

    Worked on functionality some more. Still need to figure out better submition

--- app.js
+++ app.js
@@ -2,25 +2,33 @@ let services = document.getElementById('services');
 let modalContent = document.getElementById('modal-content');
 let getStarted = document.getElementById('get-started');
 let classesInfo = document.getElementById('classes-info');
-let submit = document.getElementById('submit');
+let submit = document.getElementById('submition');
 let actionContainer = document.getElementById('action-container');
+let rotateArrow = document.getElementById('rotate-arrow');
+let confirmationMessage = document.getElementById('confirmation-message');
+let close = document.getElementById('close');
+let infoForm = document.getElementById('info-form');
 
-services.addEventListener('mouseover', function () {
-	modalContent.style.display = 'block';
-});
+services.onclick = function () {
+	if (modalContent.style.display !== 'block') {
+		modalContent.style.display = 'block';
+		rotateArrow.classList.add('rotate-arrow');
+	} else {
+		modalContent.classList.add('fadeout');
+		rotateArrow.classList.remove('rotate-arrow');
 
-// services.addEventListener('mouseleave', function () {
-// 	modalContent.classList.add('fadeout');
-// 	modalContent.style.opacity = 0;
-// 	setTimeout(function () {
-// 		modalContent.style.display = 'none';
-// 		modalContent.classList.remove('fadeout');
-// 		modalContent.style.opacity = 1;
-// 	}, 500);
-// });
+		modalContent.style.opacity = 0;
+		setTimeout(function () {
+			modalContent.style.display = 'none';
+			modalContent.classList.remove('fadeout');
+			modalContent.style.opacity = 1;
+		}, 500);
+	}
+};
 
 modalContent.addEventListener('mouseleave', function () {
 	modalContent.classList.add('fadeout');
+	rotateArrow.classList.remove('rotate-arrow');
 
 	modalContent.style.opacity = 0;
 	setTimeout(function () {
@@ -35,13 +43,19 @@ getStarted.onclick = function () {
 	actionContainer.style.display = 'none';
 };
 
-// submit.onclick = function () {
-// 	classesInfo.classList.add('fadeout');
+submit.onclick = function () {
+	confirmationMessage.style.display = 'block';
+	classesInfo.classList.add('fadeout');
 
-// 	classesInfo.style.opacity = 0;
-// 	setTimeout(function () {
-// 		classesInfo.style.display = 'none';
-// 		modalContent.classList.remove('fadeout');
-// 		classesInfo.style.opacity = 1;
-// 	}, 500);
-// };
+	classesInfo.style.opacity = 0;
+	setTimeout(function () {
+		classesInfo.style.display = 'none';
+		classesInfo.classList.remove('fadeout');
+		classesInfo.style.opacity = 1;
+	}, 500);
+};
+
+close.onclick = function () {
+	confirmationMessage.style.display = 'none';
+	infoForm.submit();
+};
